89dd6283e4e563e61fcf5d07f2c2d2544724f825
NaPohybel <warinopl@gmail.com>
2020-08-30 01:20:42 +0200

    restarted with cookiecutter

---
+++ wedding/wedding/wedding/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from wedding.users.models import User
+from wedding.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
