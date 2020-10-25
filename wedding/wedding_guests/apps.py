4198d941b679b92f0964cecf7f224d4a0d14a399
Marcin Popławski <marpop8@gmail.com>
2020-10-25 11:08:25 +0100

    Add wedding_guests to settings

--- wedding/wedding_guests/apps.py
+++ wedding/wedding_guests/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class WeddingGuestsConfig(AppConfig):
-    name = 'wedding_guests'
+    name = "wedding.wedding_guests"
+    verbose_name = _("Wedding Guests")
